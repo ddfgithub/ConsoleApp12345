cfe7bb245b6c0ed967006dfe68397080ab1e07b6
Administrator <Administrator@CNU0352RP0>
2017-11-02 10:14:52 -0400

    Add project files.

---
+++ ConsoleApp12345/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp12345
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
